c85f5e4d4d8a976a7074ee9792d09397ddd55c61
Capsalt <hbasri2304@gmail.com>
2022-03-31 10:40:22 +0300

    v2

---
+++ NLayerApp/NLayerApp/NLayer.API/Controllers/ProductsController.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core;
+using NLayer.Core.DTOs;
+using NLayer.Core.Services;
+using NLayer.Service.Utility;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayer.API.Controllers
+{
+    
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+        
+
+        public ProductsController(IService<Product> service, IMapper mapper)
+        {
+            _mapper = mapper;
+            _service = service;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            
+            var products = await _service.GetAllAsync();
+
+            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+
+            MyLogger.GetInstance().Info("all users");
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+
+            var productsDto = _mapper.Map<ProductDto>(product);
+
+            MyLogger.GetInstance().Info("get user");
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productsDto));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            var product = await _service.AddAsync(_mapper.Map<Product>(productDto));
+
+            var productsDto = _mapper.Map<ProductDto>(product);
+
+            MyLogger.GetInstance().Info("create user");
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(201, productsDto));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto productDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Product>(productDto));
+
+
+            MyLogger.GetInstance().Info("update user");
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+
+            await _service.RemoveAsync(product);
+
+            MyLogger.GetInstance().Info("delete user");
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+        }
+    }
+}
---
+++ NLayerApp/NLayerApp/NLayer.Service/Utility/MyLogger.cs
@@ -0,0 +1,67 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Utility
+{
+    public class MyLogger : ILogger
+    {
+
+        private static MyLogger instance;
+        private static Logger logger;
+
+        public MyLogger()
+        {
+
+        }
+
+        public static MyLogger GetInstance()
+        {
+            if (instance == null)
+                instance = new MyLogger();
+            return instance;
+        }
+
+        private Logger GetLogger(string theLogger)
+        {
+            if (MyLogger.logger == null)
+                MyLogger.logger = LogManager.GetLogger(theLogger);
+            return MyLogger.logger;
+        }
+
+        public void Debug(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger("NLayer.*").Debug(message);
+            else
+                GetLogger("NLayer.*").Debug(message,arg);
+        }
+
+        public void Error(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger("NLayer.*").Error(message);
+            else
+                GetLogger("NLayer.*").Error(message, arg);
+        }
+
+        public void Info(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger("NLayer.*").Info(message);
+            else
+                GetLogger("NLayer.*").Info(message, arg);
+        }
+
+        public void Warning(string message, string arg = null)
+        {
+            if (arg == null)
+                GetLogger("NLayer.*").Warn(message);
+            else
+                GetLogger("NLayer.*").Warn(message, arg);
+        }
+    }
+}
